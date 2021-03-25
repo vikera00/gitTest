077b10a3989284c006958771869aebafd94166d2
vikera00 <vikker26@gmail.com>
2021-03-25 14:39:39 +0500

    function spam check

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function ucFirst(str) {
 }
 
 ucFirst("monkey");
+
+
+function checkSpam(str) {
+    let lowStr = str.toLowerCase();
+
+    return lowStr.includes("viagra") || lowStr.includes("xxx")
+}
+
+alert( checkSpam('buy ViAgRA now') );
+alert( checkSpam('free xxxxx') );
+alert( checkSpam("innocent rabbit") );
